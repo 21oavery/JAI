585da823fe0d7f71acefcd1cbc583384da4ec547
powerboat9-school <33042784+powerboat9-school@users.noreply.github.com>
2018-12-07 14:15:07 -0500

    Create Fraction.java

---
+++ Fraction.java
@@ -0,0 +1,9 @@
+public class Fraction implements ExpressionBase {
+    private ExpressionBase n, d;
+    
+    public Fraction(ExpressionBase nIn, ExpressionBase dIn) {
+        n = nIn;
+        d = dIn;
+    }
+    
+    public 
